86c1cec61159a463bd2a89aae6fa8be6fecbd88d
trishna15 <77284959+trishna15@users.noreply.github.com>
2021-04-04 20:47:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var drops =[];
+var maxDrops = 100;
+var umbrella;
+var rand;
+var night;
+var thunder,thunder1,thunder2,thunder3,thunder4;
+var thunderCreatedFrame = 0;
+
+function preload(){
+
+   thunder1 = loadImage("images/thunderbolt/1.png");
+   thunder2 = loadImage("images/thunderbolt/2.png");
+   thunder3 = loadImage("images/thunderbolt/3.png");
+   thunder4 = loadImage("images/thunderbolt/4.png");
+
+    
+}
+
+function setup(){
+   var canvas =  createCanvas(500,700);
+
+   engine = Engine.create;
+   world = engine.world;
+
+   umbrella = new Umbrella(200,500);
+
+   for(var i = 0; i < maxDrops; i++){
+       drops.push(new createDrop (random(0,400),random(0,400)));
+   }
+    
+ 
+
+}
+
+function draw(){
+Engine.update();
+backgroud(0,0,0);
+
+rand = Math.round(random(1,4));
+if(frameCount%80 ===0){
+   thunderCreaterFrame = frameCount;
+   thunder = createSprite(random(10,370),random(10,30),10,10);
+   switch(rand){
+      case 1:thunder.addImage(thunder1);
+      break;
+      case 2:thunder.addImage(thunder2);
+      break;
+      case 3:thunder.addImage(thunder3);
+      break;
+      case 4:thunder.addImage(thunder4);
+      break;
+      default: break;
+   }
+
+   thunder.scale = 0.7;
+
+}
+
+if(thunderCreatedFrame + 10 === frameCount && thunder){
+   thunder.destroy();
+}
+
+umbrella.display();
+
+for(var i = 0; i < maxDrops; i++){
+   drops[i].display();
+   drops[i].update();
+}
+
+    
+}   
+
